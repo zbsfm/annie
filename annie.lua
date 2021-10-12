27644f333e8f872ae79efd6be8c12bf62c6766da
z j b-s <56085339+zbsfm@users.noreply.github.com>
2021-10-12 17:13:10 -0400

    Update annie.lua

--- annie.lua
+++ annie.lua
@@ -3,27 +3,6 @@ local TextureC = include "mi-eng/lib/TextureC_engine"
 
 engine.name = "TextureC"
 
-local pitch = 0 -- (+-48.0)
-local position = 0.6 -- (0 -- 1)
-local size = 0.2 -- (0 -- 1)
-local dens = 0.25 -- (0 -- 1)
-local texture = 0.1 -- (0 -- 1)
-local drywet = 0.5 -- (0 -- 1)
-local in_gain = 2 -- 0.125 -- 8
-local spread = 1 -- (0 -- 1)
-local rvb = 0.2 -- (0 -- 1)
-local feedback = 0.2 -- (0 -- 1)
-local freeze = 0 -- (0 -- 1)
-local mode = 0 -- (0 -- 3)
-local lofi = 0 -- (0 -- 1)
-local trig = 0 -- (0 -- 1)
-
-local controls = {}
-local param_assign = {"pitch","position","grainsize","density","texture","drywet","in_gain","reverb","spread","feedback","freeze","mode","lofi"}
-local clouds_mode = {"Granular","Stretch","Looping_Delay","Spectral"}
-local legend = ""
-local defualt_midicc = 32
-
 function init()
 
   -- Add params
@@ -31,124 +10,107 @@ function init()
   TextureC.add_lfo_params()
 
   -- initialize params
-  params:set("pitch", pitch)
-  params:set("position",position)
-  params:set("grainsize",size)
-  params:set("density",dens)
-  params:set("texture",texture)
-  params:set("drywet",drywet)
-  params:set("in_gain",in_gain)
-  params:set("reverb",rvb)
-  params:set("spread",spread)
-  params:set("feedback",feedback)
-  params:set("freeze",freeze)
-  params:set("mode",mode)
-  params:set("lofi",lofi)
-  params:set("trig",trig)
+  params:set("pitch", 0)
+  params:set("position",0.5)
+  params:set("grainsize",0)
+  params:set("density",0.5)
+  params:set("texture",0.5)
+  params:set("drywet",0.5)
+  params:set("in_gain",0.5)
+  params:set("reverb",0)
+  params:set("spread",1)
+  params:set("feedback",0)
+  params:set("freeze",1)
+  params:set("mode",0)
+  params:set("lofi",0)
+  params:set("trig",0)
 
-  legend = clouds_mode[params:get("mode")+1]
-
-  -- UI controls
-  controls = {}
-  controls.pitch = {ui = nil, midi = nil,}
-  controls.position = {ui = nil, midi = nil,}
-  controls.grainsize = {ui = nil, midi = nil,}
-  controls.density = {ui = nil, midi = nil,}
-  controls.texture = {ui = nil, midi = nil,}
-  controls.drywet = {ui = nil, midi = nil,}
-  controls.in_gain = {ui = nil, midi = nil,}
-  controls.reverb = {ui = nil, midi = nil,}
-  controls.spread = {ui = nil, midi = nil,}
-  controls.feedback = {ui = nil, midi = nil,}
-  controls.freeze = {ui = nil, midi = nil,}
-  controls.mode = {ui = nil, midi = nil,}
-  controls.lofi = {ui = nil, midi = nil,}
-
-
-
-  -- UI
-  local row1 = 12
-  local row2 = 30
-  local row3 = 48
-
-  local offset = 19
-  local col1 = 3
-  local col2 = col1+offset
-  local col3 = col2+offset
-  local col4 = col3+offset
-  local col5 = col4+offset
-  local col6 = col5+offset
-  local col7 = col6+offset
-
-  local col8 = 116
-  
-  controls.pitch.ui = UI.Dial.new(col1, row1, 10, 0, 0, 1, 0.01, 0, {},"", "pit")
-  controls.position.ui = UI.Dial.new(col2, row1, 10, 0, 0, 1, 0.01, 0, {},"", "pos")
-  controls.grainsize.ui = UI.Dial.new(col3, row1, 10, 0, 0, 1, 0.01, 0, {},"", "size")
-  controls.density.ui = UI.Dial.new(col4, row1, 10, 0, 0, 1, 0.01, 0, {},"", "den")
-  controls.texture.ui = UI.Dial.new(col5, row1, 10, 0, 0, 1, 0.01, 0, {},"", "tex")
-
-  controls.drywet.ui = UI.Dial.new(col1, row2, 10, 0, 0, 1, 0.01, 0, {},"", "d-w")
-  controls.in_gain.ui = UI.Dial.new(col2, row2, 10, 0, 0, 1, 0.01, 0, {},"", "gain")
-  controls.reverb.ui = UI.Dial.new(col3, row2, 10, 0, 0, 1, 0.01, 0, {},"", "verb")
-  controls.spread.ui = UI.Dial.new(col4, row2, 10, 0, 0, 1, 0.01, 0, {},"", "sprd")
-  controls.feedback.ui = UI.Dial.new(col5, row2, 10, 0, 0, 1, 0.01, 0, {},"", "fb")
-
-  controls.freeze.ui = UI.Dial.new(col1, row3, 10, 0, 0, 1, 1, 0, {},"", "frz")
-  controls.lofi.ui = UI.Dial.new(col2, row3, 10, 0, 0, 1, 1, 0, {},"", "lofi")
-  controls.mode.ui = UI.Dial.new(col4+10, row3, 10, 0, 0, 3, 1, 0, {},"", "mode")
-
-  for k,v in pairs(controls) do
-     controls[k].ui:set_value (params:get(k))
-  end  
-  
-  
   redraw()
 end
 
-function key(n,z)
-  -- key actions: n = number, z = state
-  if n == 2 and z == 1 then
-    engine.freeze(1)
-  else
-    engine.freeze(0)
+keys = {0, 0, 0}
+function key(k,z)
+  keys[k] = z
+  if z == 1 then
+    if keys[2] == 1 and keys[3] == 0 then -- just k2?
+      print("k2 pressed")
+      active_group = util.wrap(active_group - 1, 1, 3)
+    elseif keys[3] == 1 and keys[2] == 0 then -- just k3?
+      print("k3 pressed")
+      active_group = util.wrap(active_group + 1, 1, 3)
+    end
+  elseif(keys[2] == 1 or keys[3] == 1) then -- both k2 and k3?
+      print("k2 and k3 pressed")
+      if params:get('freeze') == 2 then params:set('freeze', 1)
+      else params:set('freeze', 2) end
   end
-   if n == 3 and z == 1 then
-    engine.trig(1)
-  else
-    engine.trig(0)
-  end
-  
+  redraw()
 end
 
 function enc(n,d)
-  -- encoder actions: n = number, d = delta
-  if n == 1 then
-    params:delta("position", d)
-    controls.position.ui:set_value (params:get("position"))
-    --print("position",string.format("%.2f", params:get("position")))
-  elseif n == 2 then
-    params:delta("grainsize", d)
-    controls.grainsize.ui:set_value (params:get("grainsize"))
-  elseif n == 3 then
-    params:delta("density", d)
-    controls.density.ui:set_value (params:get("density"))
-    --print("density",string.format("%.2f", params:get("density")))
---  elseif n == 4 then
---    params:delta("drywet", d)
-    --print("density",string.format("%.2f", params:get("drywet")))
-  end
+  last_delta = param_names[(active_group * 3 - 2) + (n - 1)]
+  params:delta(last_delta, d)
   redraw()
 end
 
+names = {'PITCH', 'DENSITY', 'SIZE', 'TEXTURE', 'MIX', 'STEREO', 'FEEDBACK', 'REVERB'}
+param_names = {'pitch', 'density', 'grainsize', 'texture', 'drywet', 'spread', 'feedback', 'reverb', 'position'}
+active_group = 1
+truth_table = {[1] = true, [2] = true, [4] = true} -- params that start from the middle
+last_delta = 'reverb'
 
 function redraw()
   -- screen redraw
   screen.clear()
+  screen.aa(1)
+  screen.line_width(3)
   
-  screen.update()
-end
+  for i=1, 8 do --vertical sliders
+    local pos = (i * 16) - 2
 
-function cleanup()
-  -- deinitialization
+    if i >= (active_group * 3 - 2) and (i <= active_group * 3) then -- are we in the active control group?
+      screen.level(7)
+    else
+      screen.level(2)
+    end
+
+    screen.text_rotate(pos - 8, 2, names[i], 90) -- draw param names
+    screen.level(2)
+    screen.move(pos, 40)
+    screen.line(pos, 2)
+    screen.stroke() -- draw param sliders
+
+
+    screen.level(15)
+    if truth_table[i] then screen.move(pos, 19) -- bipolar param?
+    else screen.move(pos, 40) end
+    
+    if i == 1 then screen.line(pos, 40 - params:get(param_names[i]) * 0.01 * 38 - 19) -- fix pitch scaling for graphic
+    else screen.line(pos, 40 - params:get(param_names[i] ) * 38) end -- draw param position bars
+    screen.stroke()
+  end
+  
+  screen.level(2) -- last touched param
+  screen.move(128, 50)
+  screen.text_right(last_delta .. ':  ' .. params:get(last_delta))
+
+  screen.level(2)
+  screen.move(0, 56) -- horizontal slider
+  screen.line(128, 56)
+  screen.stroke()
+
+  screen.level(15)
+  screen.move(2 + params:get('position') * 124 - 2, 56) -- horizontal indicator
+  screen.line_rel(4, 0)
+  screen.stroke()
+
+  screen.move(4, 64)
+  if active_group == 3 then screen.level(15)
+  else screen.level(2) end
+  screen.text("POSITION") -- POSITION text
+
+  screen.move(124, 64)
+  if params:get('freeze') == 2 then screen.level(15); screen.text_right("FROZEN!") end
+
+  screen.update()
 end
